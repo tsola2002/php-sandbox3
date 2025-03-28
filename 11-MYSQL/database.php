9ca40a3f24fb0caf3290a3a00ae2a7590f9e2298
Omatsola Isaac Sobotie <tsola2002@yahoo.co.uk>
2025-03-28 14:01:24 +0100

    run remaining crud functions

--- 11-MYSQL/database.php
+++ 11-MYSQL/database.php
@@ -11,7 +11,7 @@
         $hostname = 'localhost';
         $dbname = 'DB_JOB';
         $username = 'root';
-        $password = 'root';
+        $password = '';
 
         // 1. this will create a connection to our database
         global $connection;
@@ -28,6 +28,48 @@
   
     $query = 'SELECT * FROM TBL_USER';
     $result = $connection->query($query);
+
+
+    function createRecord($id, $name, $qualification, $experience){
+        global $connection;
+        $insertQuery = "INSERT INTO TBL_USER(userid, name, qualification, experience) VALUES ('$id', '$name', '$qualification', '$experience')";
+
+        //executing the query
+        return $connection->query($insertQuery);
+    }
+
+    function updateRecord($id, $name, $qualification, $experience){
+        global $connection;
+        $updateQuery = "UPDATE TBL_USER SET userid = '$id',
+                                name = '$name',
+                                qualification = '$qualification',
+                                experience = '$experience'
+                                WHERE userid = '$id'";
+
+        //executing the query
+        return $connection->query($updateQuery);
+    }
+
+    function deleteRecord($id){
+        global $connection;
+        $deleteQuery = "DELETE FROM TBL_USER WHERE userid = '$id'";
+
+        //executing the query
+        return $connection->query($deleteQuery);
+    }
+
+
+
+    // FUNCTION USAGE
+    //createRecord(6, "Dasoka", "Msc", 15);
+
+    // Updating a record 
+    // updateRecord(1, 'Omatsola', 'PG/DIP', 17);
+
+    //delete a record 
+    deleteRecord(6);
+
+
 
     echo "<table border='1'>";
     echo "<tr><th>User ID</th><th>Name</th><th>Qualification</th><th>Experience</th></tr>";
